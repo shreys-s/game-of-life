52497eca5af0354601e944f17bda71d4fed9995b
shreys-s <shrey.careers@gmail.com>
2019-11-07 11:52:40 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,15 +1,15 @@
 def call(Map stageParams) {
 script
 {
-					if (env.BRANCH_NAME == 'featurepraveen' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
-					{
-						echo "Running build from branch ${env.BRANCH_NAME}"
-						sh 'mvn clean install'
-						//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
-					}
-					else
-					{
-						echo 'The repository/branch name does not meet Clorox branch naming standard'
-					}
+	if (env.BRANCH_NAME == 'featurepraveen' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
+	{
+		echo "Running build from branch ${env.BRANCH_NAME}"
+		sh 'mvn clean install'
+		//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
 	}
-  }
+	else
+	{
+		echo 'The repository/branch name does not meet Clorox branch naming standard'
+	}
+}
+}
