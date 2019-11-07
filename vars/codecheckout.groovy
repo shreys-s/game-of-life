609769b457e69207cde5852ffaea889e021ed1ab
shreys-s <shrey.careers@gmail.com>
2019-11-07 11:23:49 +0530

    Update and rename codecheckout.groovy to gitCheckout.groovy

--- vars/codecheckout.groovy
+++
@@ -1,4 +0,0 @@
-def code_checkout(branch,url) 
-{ 
-    checkout([$class: 'GitSCM', branches: [[name: '$branch']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Github_cred', url: '$url']]])
-}
