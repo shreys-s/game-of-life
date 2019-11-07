0c93a36db84965604287c89789a033018d8aea06
shreys-s <shrey.careers@gmail.com>
2019-11-07 12:12:05 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,33 +1,7 @@
 def call()
 {
-	pipeline
-{
-	agent { label "master" }
-	environment
+	stage('Code Build')
 	{
-		ANYPOINT_CREDENTIALS = credentials('anypoint.credentials.nonprod')
+		sh 'mvn clean install'
 	}
-	stages
-	{
-		stage('Code Build')
-		{
-			steps
-			{
-				script
-				{
-					if (env.BRANCH_NAME == 'featurepraveen' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
-					{
-						echo "Running build from branch ${env.BRANCH_NAME}"
-						sh 'mvn clean install'
-						//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
-					}
-					else
-					{
-						echo 'The repository/branch name does not meet Clorox branch naming standard'
-					}
-				}
-			}
-		}
-	}
-}
 }
