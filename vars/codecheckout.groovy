0d8ad3fc515b6ffabdf5fcedab48b6b5e55b83b3
shreys-s <shrey.careers@gmail.com>
2019-11-07 11:14:44 +0530

    Update codecheckout.groovy

--- vars/codecheckout.groovy
+++ vars/codecheckout.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams) 
+def code_checkout(branch,url) 
 { 
-    checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]],doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs:[[credentialsId: '9a71c103-12b0-41c9-8375-90af30de786b', url: stageParams.url]]])
+    checkout([$class: 'GitSCM', branches: [[name: '$branch']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Github_cred', url: '$url']]])
 }
