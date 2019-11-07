6d56c2ab4cbbb283e80ce49def7eb3d7afec2827
shreys-s <shrey.careers@gmail.com>
2019-11-07 12:05:37 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,13 +1,32 @@
-def call(body) {
-if (env.BRANCH_NAME == 'featurepraveen' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
+def call()
+{
+	pipeline
+{
+	agent { label "sjeapvl02" }
+	environment
 	{
-		echo "Running build from branch ${env.BRANCH_NAME}"
-		sh 'mvn clean install'
-		//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
+		ANYPOINT_CREDENTIALS = credentials('anypoint.credentials.nonprod')
 	}
-	else
+	stages
 	{
-		echo 'The repository/branch name does not meet Clorox branch naming standard'
+		stage('Code Build')
+		{
+			steps
+			{
+				script
+				{
+					if (env.BRANCH_NAME == 'develop' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
+					{
+						echo "Running build from branch ${env.BRANCH_NAME}"
+						sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
+					}
+					else
+					{
+						echo 'The repository/branch name does not meet Clorox branch naming standard'
+					}
+				}
+			}
+		}
 	}
-	return this
+}
 }
