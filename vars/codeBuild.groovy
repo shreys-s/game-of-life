4d127afb8917137ca8ae4d14e8f200b2eb3f51ae
shreys-s <shrey.careers@gmail.com>
2019-11-07 12:19:19 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -2,21 +2,15 @@ def call()
 {
 stage('Code Build')
 {
-	steps
+	if (env.BRANCH_NAME == 'develop' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
 	{
-		script
-		{
-			if (env.BRANCH_NAME == 'develop' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
-			{
-				echo "Running build from branch ${env.BRANCH_NAME}"
-				sh 'mvn clean install'
-				//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
-			}
-			else
-			{
-				echo 'The repository/branch name does not meet Clorox branch naming standard'
-			}
-		}
+		echo "Running build from branch ${env.BRANCH_NAME}"
+		sh 'mvn clean install'
+		//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
+	}
+	else
+	{
+		echo 'The repository/branch name does not meet Clorox branch naming standard'
 	}
 }
 }
