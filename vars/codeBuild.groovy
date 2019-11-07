8f04dfb324a05f493c07f524c731dd7e29775082
shreys-s <shrey.careers@gmail.com>
2019-11-07 11:38:29 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,10 +1,11 @@
 def call(Map stageParams) {
 script
 {
-					if (env.BRANCH_NAME == 'develop' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
+					if (env.BRANCH_NAME == 'featurepraveen' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('release'))
 					{
 						echo "Running build from branch ${env.BRANCH_NAME}"
-						sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
+						sh 'mvn clean install'
+						//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
 					}
 					else
 					{
