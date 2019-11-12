850ef3e37c12d3721dabb1424e3adb8774d956c3
shreys-s <shrey.careers@gmail.com>
2019-11-12 14:49:38 +0530

    Update muleCicd.groovy

--- vars/muleCicd.groovy
+++ vars/muleCicd.groovy
@@ -1,18 +1,5 @@
 #!/usr/bin/env groovy
-import gitCheckout
-pipeline 
+def call(branch)
 {
-    agent { label "master" }
-    stages 
-    {
-        stage('Git Checkout') 
-        {
-            steps 
-            {
-                gitCheckout(
-                branch: "featurepraveen",
-                url: "https://github.com/shreys-s/game-of-life.git")
-            }
-        }
-    }
+    bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
 }
