bd2d29ed6bc72dcce5efd944fd5eb606915122ad
shreys-s <shrey.careers@gmail.com>
2019-11-12 14:32:47 +0530

    Update and rename MuleCICD.groovy to muleCicd.groovy

--- vars/MuleCICD.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-def call(String name = 'human') 
-{
-	if (${name} == 'develop' || ${name}.startsWith('feature') || ${name} == 'master'))
-	{
-		bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
- 	}
-}
---
+++ vars/muleCicd.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+def call() 
+{
+	bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
+}
