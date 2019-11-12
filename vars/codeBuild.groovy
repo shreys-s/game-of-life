9d21d4ce7e90db80c81d7debfd985f7c0c6db958
shreys-s <shrey.careers@gmail.com>
2019-11-12 10:07:07 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,8 +1,3 @@
-def call()
-{
-stage('Code Build')
-{
-	sh 'mvn clean install'
-		//sh 'mvn clean install -Dv=${BUILD_NUMBER} -Du=${ANYPOINT_CREDENTIALS_USR} -Dp=${ANYPOINT_CREDENTIALS_PSW}'
-}
+def ref() {
+    return sh(script: 'mvn clean install ', returnStdout: true).trim()
 }
