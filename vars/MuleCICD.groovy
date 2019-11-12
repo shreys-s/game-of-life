5328d97f87197254617529d8ba8ae0d416c72714
shreys-s <shrey.careers@gmail.com>
2019-11-12 14:30:21 +0530

    Update MuleCICD.groovy

--- vars/MuleCICD.groovy
+++ vars/MuleCICD.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call(String name = 'human') 
 {
-	if (${name} == 'develop' || ${name}.startsWith('feature'))
+	if (${name} == 'develop' || ${name}.startsWith('feature') || ${name} == 'master'))
 	{
 		bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
  	}
