c3d24dea0a4402f84cccaa2868db0f4f044793ee
shreys-s <shrey.careers@gmail.com>
2019-11-12 15:06:52 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 def call(branch)
 {
-    bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
+    if (env.BRANCH_NAME == 'develop' || env.BRANCH_NAME.startsWith('feature') || env.BRANCH_NAME.startsWith('master'))
+    {
+        bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
+    }
+    else
+    {
+        println 'The repository/branch name does not meet Clorox branch naming standard'
+    }
 }
