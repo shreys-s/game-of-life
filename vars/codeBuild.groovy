5c2cb47da93fee5a45dffa371ae448fa89345d82
shreys-s <shrey.careers@gmail.com>
2019-11-12 15:12:41 +0530

    Update codeBuild.groovy

--- vars/codeBuild.groovy
+++ vars/codeBuild.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call(branch)
 {
-    if (env.BRANCH_NAME == 'master')
+    if (env.branch == 'master')
     {
         bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
     }
