69ed1d783e04ab34f0aaee109fcdeca74a5b0898
shreys-s <shrey.careers@gmail.com>
2019-11-12 11:49:28 +0530

    Update GlobalVars.groovy

--- vars/GlobalVars.groovy
+++ vars/GlobalVars.groovy
@@ -1,11 +1,5 @@
 #!/usr/bin/env groovy
-package com.cleverbuilder
 
 class GlobalVars {
    static String foo = "bar"
-
-   // refer to this in a pipeline using:
-   //
-   // import com.cleverbuilder.GlobalVars
-   // println GlobalVars.foo
 }
