bad67e846b060e3fa80b64c7559fca29788e046c
shreys-s <shrey.careers@gmail.com>
2019-11-12 14:39:23 +0530

    Update muleCicd.groovy

--- vars/muleCicd.groovy
+++ vars/muleCicd.groovy
@@ -16,7 +16,3 @@ pipeline
         }
     }
 }
-def call() 
-{
-	bat "E:\\apache-maven-3.6.2\\bin\\mvn -f pom.xml clean test package verify checkstyle:checkstyle pmd:pmd findbugs:findbugs javadoc:javadoc cobertura:cobertura -Pmetrics"
-}
